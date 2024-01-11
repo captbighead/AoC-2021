f871cb0f66d9fa5ccebaf0a32ac8b8b041fd83b8
captbighead <ajt2cbhds@gmail.com>
2024-01-10 16:21:53 -0800

    Started 15

--- solutions/day15.py
+++ solutions/day15.py
@@ -14,6 +14,11 @@ except:
 	input_lines = ["Input Lines Not Found"]
 	example_lines = ["Example"]
 
+class Graph: 
+
+	def __init__(self, line_defs, extended) -> None:
+		pass
+
 def do_part_one_for(lines):
 	pass
 
@@ -24,13 +29,13 @@ def solve_p1():
 	print(f"PART ONE\n--------\n")
 	print(f"This is the prompt for Part One of the problem.\n")
 
-	print(f"When we do part one for the example input:")
 	results = do_part_one_for(example_lines)
+	print(f"When we do part one for the example input:")
 	print(f"\tThe <THING THEY WANT> is {results}")
 	print(f"\tWe expected: <SOLUTION THEY WANT>\n")
 
-	print(f"When we do part one for the actual input:")
 	results = do_part_one_for(input_lines)
+	print(f"When we do part one for the actual input:")
 	print(f"\tThe <THING THEY WANT> is {results}\n")
 
 def solve_p2():
@@ -38,13 +43,13 @@ def solve_p2():
 	print(f"PART TWO\n--------\n")
 	print(f"This is the prompt for Part Two of the problem.\n")
 
-	print(f"When we do part two for the example input:")
 	results = do_part_two_for(example_lines)
+	print(f"When we do part two for the example input:")
 	print(f"\tThe <THING THEY WANT> is {results}")
 	print(f"\tWe expected: <SOLUTION THEY WANT>\n")
 
-	print(f"When we do part two for the actual input:")
 	results = do_part_two_for(input_lines)
+	print(f"When we do part two for the actual input:")
 	print(f"\tThe <THING THEY WANT> is {results}\n")
 
 def print_header():
