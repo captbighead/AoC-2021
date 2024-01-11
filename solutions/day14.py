7cb033d6474c4cddd1e8acd3b6179aa249bb973e
captbighead <ajt2cbhds@gmail.com>
2024-01-10 16:21:27 -0800

    Solved 14

--- solutions/day14.py
+++ solutions/day14.py
@@ -14,37 +14,66 @@ except:
 	input_lines = ["Input Lines Not Found"]
 	example_lines = ["Example"]
 
-def do_part_one_for(lines):
-	pass
+def do_part_soln_for(lines, step_count):
+	start = lines[0]
+	LEFTMOST, RIGHTMOST = (start[0], start[-1])
+	pairs = defaultdict(int)
+	for i in range(len(start)-1):
+		pairs[start[i:i+2]] += 1
+	
+	reactions = {}
+	for reaction_def in lines[2:]:
+		a, b, c = algos.erase(reaction_def, [" -> "])
+		reactions[f"{a}{b}"] = (f"{a}{c}", f"{c}{b}")
+	
+	for step in range(step_count):
+		new_pairs = defaultdict(int)
+		for pair, pcount in pairs.items():
+			left, right = reactions[pair]
+			new_pairs[left] += pcount
+			new_pairs[right] += pcount
+		pairs = new_pairs
 
-def do_part_two_for(lines):
-	pass
+	# Now that we've done all the stepping: Every instance of a pair has one of 
+	# two repetitions of it's element, except for the leading pair and the final
+	# pair. So, count the number of times each element appears in the pairs that
+	# are in our final string, and integer divide them by 2. Add 1 back for the
+	# head element and the tail element.
+	element_counts = defaultdict(int)
+	for pair, pcount in pairs.items():
+		a, b = pair
+		element_counts[a] += pcount
+		element_counts[b] += pcount
+	for element in element_counts:
+		element_counts[element] //= 2
+		if element in (LEFTMOST, RIGHTMOST):
+			element_counts[element] += 1
+	return max(element_counts.values()) - min(element_counts.values())
 
 def solve_p1():
 	print(f"PART ONE\n--------\n")
 	print(f"This is the prompt for Part One of the problem.\n")
 
+	results = do_part_soln_for(example_lines, 10)
 	print(f"When we do part one for the example input:")
-	results = do_part_one_for(example_lines)
 	print(f"\tThe <THING THEY WANT> is {results}")
-	print(f"\tWe expected: <SOLUTION THEY WANT>\n")
+	print(f"\tWe expected: 1588\n")
 
+	results = do_part_soln_for(input_lines, 10)
 	print(f"When we do part one for the actual input:")
-	results = do_part_one_for(input_lines)
 	print(f"\tThe <THING THEY WANT> is {results}\n")
 
 def solve_p2():
-	return
 	print(f"PART TWO\n--------\n")
 	print(f"This is the prompt for Part Two of the problem.\n")
 
+	results = do_part_soln_for(example_lines, 40)
 	print(f"When we do part two for the example input:")
-	results = do_part_two_for(example_lines)
 	print(f"\tThe <THING THEY WANT> is {results}")
-	print(f"\tWe expected: <SOLUTION THEY WANT>\n")
+	print(f"\tWe expected: 2188189693529\n")
 
+	results = do_part_soln_for(input_lines, 40)
 	print(f"When we do part two for the actual input:")
-	results = do_part_two_for(input_lines)
 	print(f"\tThe <THING THEY WANT> is {results}\n")
 
 def print_header():
